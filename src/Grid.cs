a0251c5020ce07c1542964a61d1554990a05f464
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-07-22 16:23:50 -0300

    docs: Adiciona documentação para classe Grid
    Adiciona documentação para os métodos da classe 'Grid', e também para algumas
    partes da implementação da classe 'Chunk'.

--- src/Grid.cs
+++ src/Grid.cs
@@ -12,8 +12,10 @@ public class Grid
 
     public Cell At((uint X, uint Y) pos) => _cells[pos.Y, pos.X];
 
+    // Inicializa o grid escolhendo posições aleatórias para conter bombas
     public Chunk()
     {
+      // Inicializa a chunk com bombas
       Random rand = new();
       for (uint bombsPlaced = 0; bombsPlaced < 16 * 16 * Grid.BOMBS_DENSITY;)
       {
@@ -32,14 +34,28 @@ public class Grid
   private Chunk _origin = new();
   private Dictionary<(int X, int Y), Chunk> _chunks = [];
 
+  /// <summary>
+  /// Retorna a chunk atribuída a uma coordenada. Se essa chunk não
+  /// existir, ela é inicializada.
+  /// </summary>
   private Chunk ChunkAt(int cx, int cy) => ChunkAt((cx, cy));
 
+  /// <summary>
+  /// Retorna a chunk atribuída a uma coordenada. Se essa chunk não
+  /// existir, ela é inicializada.
+  /// </summary>
   private Chunk ChunkAt((int X, int Y) chunkPosition)
   {
     _chunks.TryAdd(chunkPosition, new Chunk());
     return _chunks[chunkPosition];
   }
 
+  /// <summary>
+  /// Retorna a célula em uma coordenada específica do mapa
+  ///
+  /// Garante que a célula vai existir, inicializando regiões
+  /// do mapa automaticamente
+  /// </summary>
   public Cell At(int x, int y)
   {
     (int X, int Y) chunkPosition = (
@@ -53,7 +69,7 @@ public class Grid
     );
 
     var chunk = ChunkAt(chunkPosition);
-    var cell = chunk.At(inChunkPosition.X, inChunkPosition.Y);
+    var cell = chunk.At(inChunkPosition);
 
     if (cell.GotInitialized)
       return cell;
