d6c11dee022e39a5c85b75cd2158e53060fca545
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-07-22 16:26:32 -0300

    Adiciona método para revelar célula
    Adiciona um método 'Reveal' para revelar a célula ao jogador.
    Também adiciona uma propriedade (apenas getter) 'IsRevealed'
    para checar se a célula foi revelada.

--- src/Cell.cs
+++ src/Cell.cs
@@ -5,10 +5,13 @@ namespace CampoMinado.Grid;
 public record class Cell
 {
   private bool _initialized = false;
+  private bool _revealed = false;
 
   public bool HaveFlag { get; set; } = false;
   public bool HaveBomb => NearBombs == -1;
   public bool GotInitialized => _initialized;
+  public bool IsRevealed => _revealed;
+
   public int NearBombs
   {
     get;
@@ -24,6 +27,16 @@ public record class Cell
   {
     _initialized = true;
     NearBombs = -1;
+  }
+
+  // Marca a célula como revelada para o jogador
+  //
+  // Uma célula que não foi inicializada não deve
+  // ser revelada
+  public void Reveal()
+  {
+    Debug.Assert(_initialized);
+    _revealed = true;
   }
 
   public Cell() { }
