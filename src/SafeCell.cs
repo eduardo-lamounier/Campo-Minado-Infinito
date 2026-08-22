17fe26cb60891373d6108e1ac8ec4d2ea7ebb194
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-08-21 23:21:57 -0300

    Usa primary constructor na definição de SafeCell

--- src/SafeCell.cs
+++ src/SafeCell.cs
@@ -6,7 +6,10 @@ namespace CampoMinado.Core;
 /// Representa uma célula sem bomba do campo minado. Armazena a quantidade células com
 /// bomba adjacentes.
 /// </summary>
-public record class SafeCell : Cell
+/// <param name="nearBombs">
+///   Representa a quantidade de bombas adjacentes. É zero por padrão.
+/// </param>
+public record class SafeCell(int nearBombs = 0) : Cell
 {
   /// <summary>
   /// Representa a quantidade de células com bomba perto dessa célula.
@@ -28,13 +31,5 @@ public record class SafeCell : Cell
 
       field = value;
     }
-  }
-
-  /// <param name="nearBombs">
-  ///   Representa a quantidade de bombas adjacentes. É zero por padrão.
-  /// </param>
-  public SafeCell(int nearBombs = 0)
-  {
-    NearBombs = nearBombs;
-  }
+  } = nearBombs;
 }
