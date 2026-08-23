c4d5ec24ab80d9864d5e51f58dce79ae1998de41
a2025954101-coder <a2025954101@teiacoltec.org>
2026-08-23 14:33:48 -0300

    Implementa lógica de jogo e renderização

--- src/MainGame.cs
+++ src/MainGame.cs
@@ -16,6 +16,8 @@ public class MainGame : Game
 
   private MineField Field { get; set; }
 
+  private MineFieldRenderer FieldRenderer { get; set; }
+
   /// <summary>
   /// Gerencia as configurações relacionadas aos gráficos e à janela do programa.
   /// </summary>
@@ -38,6 +40,31 @@ public class MainGame : Game
   /// </summary>
   public new ContentManager Content { get; private set; }
 
+  private (int X, int Y) _currentPosition = (0,0);
+
+  private const uint _baseSpriteSize = 16; // Todo sprite é 16x16 pixels
+  private const float _spriteScaling = 2;
+  private static uint SpriteSize => (uint)(_baseSpriteSize * _spriteScaling);
+
+  private KeyboardState _previousKeyboardState = new();
+  private MouseState _previousMouseState = new();
+
+  private bool _gameOver = false;
+  
+  private void RevealCellWithZeroNearBombs(int x, int y)
+  {
+    Field.At(x, y).Reveal();
+
+    for(int dx = -1; dx <= 1; dx++)
+      for(int dy = -1; dy <= 1; dy++) {
+        var cell = (SafeCell)Field.At(x + dx, y + dy);
+        if (cell.NearBombs == 0 && !cell.IsRevealed)
+          RevealCellWithZeroNearBombs(x + dx, y + dy);
+        else
+          cell.Reveal();
+      }
+  }
+
   /// <summary>
   /// Método chamado a todo frame. Contém lógica do programa que tem que ser
   /// atualizada a todo momento.
@@ -48,9 +75,12 @@ public class MainGame : Game
   /// </remarks>
   protected override void Update(GameTime gameTime)
   {
+    if(_gameOver)
+      Exit();
+
     if (
-      GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
-      || Keyboard.GetState().IsKeyDown(Keys.Escape)
+        GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+        || Keyboard.GetState().IsKeyDown(Keys.Escape)
     )
       Exit();
 
@@ -60,15 +90,118 @@ public class MainGame : Game
      */
 
     Console.Clear();
+
     Console.WriteLine("Tempo passado: " + gameTime.TotalGameTime);
+
     var fps = TimeSpan.FromSeconds(1) / gameTime.ElapsedGameTime;
     Console.WriteLine("FPS (aproximado): " + fps);
 
-    // TODO: Implementar lógica de jogo: revelação de células, posicionamento de bandeiras
-    // etc
+    // =========================
+    // MOVIMENTAÇÃO DO MAPA
+    // =========================
+
+    KeyboardState keyboard = Keyboard.GetState();
+
+    if (_previousKeyboardState.IsKeyUp(Keys.W)
+      || _previousKeyboardState.IsKeyUp(Keys.A)
+      || _previousKeyboardState.IsKeyUp(Keys.S) 
+      || _previousKeyboardState.IsKeyUp(Keys.D)
+    )
+    {
+      if (keyboard.IsKeyDown(Keys.W))
+      {
+        _currentPosition.Y += (int)(Window.ClientBounds.Height / SpriteSize);
+      }
+      else if (keyboard.IsKeyDown(Keys.A))
+      {
+        _currentPosition.X -= (int)(Window.ClientBounds.Width / SpriteSize);
+      }
+      else if (keyboard.IsKeyDown(Keys.S))
+      {
+        _currentPosition.Y -= (int)(Window.ClientBounds.Height / SpriteSize);
+      }
+      else if (keyboard.IsKeyDown(Keys.D))
+      {
+        _currentPosition.X += (int)(Window.ClientBounds.Width / SpriteSize);
+      }
+      _previousKeyboardState = Keyboard.GetState();
+    }
+
+    Console.WriteLine(
+        "Posição atual (x, y): "
+        + _currentPosition.X + ", "
+        + _currentPosition.Y
+    );
+
+    // =========================
+    // MOUSE
+    // =========================
+
+    MouseState mouse = Mouse.GetState();
+
+    int x = _currentPosition.X + mouse.X / (int)SpriteSize;
+    int y = _currentPosition.Y - mouse.Y / (int)SpriteSize;
+
+    if (mouse.LeftButton == ButtonState.Pressed &&
+        _previousMouseState.LeftButton == ButtonState.Released)
+    {
+      Cell cell = Field.At(x, y);
+
+      if(!cell.HasFlag && cell is DangerousCell)
+      {
+        // Gameover
+        _gameOver = true;
+
+        // Revela todas as células atualmente visíveis:
+        foreach (var c in Field.GetRegion(
+                            _currentPosition.X,
+                            _currentPosition.Y,
+                            (uint)Window.ClientBounds.Width / SpriteSize,
+                            (uint)Window.ClientBounds.Height / SpriteSize
+                          )
+        )
+          c.Reveal();
+
+        Console.WriteLine("GAME OVER!");
+        return;
+      }
+
+      if (!cell.IsRevealed && !cell.HasFlag)
+      {
+        var safeCell = (SafeCell)cell;
+        if (safeCell.NearBombs == 0)
+          RevealCellWithZeroNearBombs(x, y);
+        else
+          safeCell.Reveal();
+      }
+    }
+
+    if (mouse.RightButton == ButtonState.Pressed &&
+        _previousMouseState.RightButton == ButtonState.Released)
+    {
+        Cell cell = Field.At(x, y);
+
+        if (!cell.IsRevealed)
+        {
+            if (cell.HasFlag)
+            {
+                // Remove a bandeira
+                cell.EnsureNoFlag();
+            }
+            else
+            {
+                // Coloca a bandeira
+                cell.EnsureFlag();
+            }
+        }
+    }
+
+    // Guarda o estado atual do mouse
+    // para detectar somente o momento do clique
+    _previousMouseState = mouse;
 
     base.Update(gameTime);
-  }
+}
 
   /// <summary>
   /// Método chamado a todo frame. Contém lógica de renderização do programa.
@@ -81,7 +214,32 @@ public class MainGame : Game
   {
     GraphicsDevice.Clear(Color.CornflowerBlue);
 
-    // TODO: Desenhar o campo minado carregado
+    uint width = (uint)Window.ClientBounds.Width; // Largura da janela
+    uint height = (uint)Window.ClientBounds.Height; // Altura da janela
+
+    uint m = height / SpriteSize; // Quant. linhas da região
+    uint n = width / SpriteSize; // Quant. colunas da região
+
+    // Região do campo minado a ser desenhada na tela:
+    Cell[,] region = Field.GetRegion(_currentPosition.X, _currentPosition.Y, n, m);
+
+    SpriteBatch.Begin(); // Começa a desenhar na tela
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        Sprite sprite = FieldRenderer.RenderCell(region[i, j])
+                          .WithScale(_spriteScaling)
+                          .WithPosition(SpriteSize*j , SpriteSize*i); 
+        sprite.Draw(SpriteBatch);
+      }
+    }
+
+    SpriteBatch.End(); // Termina de desenhar na tela
+
+    // Sprite spriteCelula = FieldRenderer.RenderCell(region[i,j]);
+    // spriteCelula.Draw(SpriteBatch);
 
     base.Draw(gameTime);
   }
@@ -138,6 +296,7 @@ public class MainGame : Game
 
     GraphicsDevice = base.GraphicsDevice;
     SpriteBatch = new SpriteBatch(GraphicsDevice);
-    Field = new();
+    Field = new(0.15);
+    FieldRenderer = new(Content);
   }
 }
