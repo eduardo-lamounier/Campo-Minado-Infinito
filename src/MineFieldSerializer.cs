de674aa98fc4709105a29307f005b06e92d6f9a0
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-08-22 23:43:41 -0300

    Melhora algumas coisas da definição de "Serializable"
    - Muda 'Serializable' para 'MineFieldSerializer' (o nome da classe);
    - Faz com que o construtor da classe sempre inicialize o campo minado;
    - Faz com que '_instance' seja nulável - já que pode ser que o singleton
    não tenha sido inicializado;
    - Lança exceção na tentativa de serializar ou desserializar sem iniciar
    a singleton;
    - Renomeia '_instance' para 's_instance', que é convenção de nome para campos
    privados ESTÁTICOS (apenas o prefixo '_' é usado em campos privados
    não-estáticos).

--- src/MineFieldSerializer.cs
+++ src/MineFieldSerializer.cs
@@ -6,17 +6,20 @@ namespace CampoMinado.src;
 /// <summary>
 /// Classe responsável pela serialização do mapa.
 /// </summary>
-public sealed class Serializable
+public sealed class MineFieldSerializer
 {
   /// <summary>
   /// Privado para não haver perigo de haver duas instâncias
   /// </summary>
-  private Serializable() { }
+  private MineFieldSerializer(MineField field)
+  {
+    Field = field;
+  }
 
   /// <summary>
   /// Única instância da classe
   /// </summary>
-  private static Serializable _instance;
+  private static MineFieldSerializer? s_instance;
 
   /// <summary>
   /// O campo que será serializado e deserializado
@@ -27,38 +30,54 @@ public sealed class Serializable
 
   private readonly string Filename = "MineField.json";
 
-    /// <summary>
-    /// Retorna a instância da classe.
-    /// Caso não houver uma instância, cria-a, se tiver retorna a instância criada na primeira vez
-    /// </summary> 
-    public static Serializable GetInstance(MineField field) 
+  /// <summary>
+  /// Retorna a instância da classe.
+  /// Caso não houver uma instância, cria-a, se tiver retorna a instância criada na primeira vez
+  /// </summary>
+  public static MineFieldSerializer GetInstance(MineField field)
+  {
+    if (s_instance == null)
     {
-        if(_instance == null)
-        {
-            _instance = new Serializable() { Field = field };
-        }
-
-        return _instance;
+      s_instance = new MineFieldSerializer(field);
     }
+
+    return s_instance;
+  }
 
   /// <summary>
   /// Serializa o mapa, adicionando ele no arquivo JSON MineField.json
   /// </summary>
-    public static void Serialize()
+  /// <exception cref="InvalidOperationException">
+  /// Lançada caso o serializador não tenha sido inicializado.
+  /// </exception>
+  public static void Serialize()
+  {
+    if (s_instance is null)
     {
-        var option = _instance.Options;
-        string serialization = JsonSerializer.Serialize(_instance.Field, option);
-
-        File.WriteAllText(_instance.Filename, serialization);
+      throw new InvalidOperationException("O serializador não foi inicializado.");
     }
-  
+
+    var option = s_instance.Options;
+    string serialization = JsonSerializer.Serialize(s_instance.Field, option);
+
+    File.WriteAllText(s_instance.Filename, serialization);
+  }
+
   /// <summary>
   /// Deserializa o campo do mapa
   /// </summary>
   /// <returns> Retorna o Campo do mapa que estava serializado </returns>
+  /// <exception cref="InvalidOperationException">
+  /// Lançada caso o serializador não tenha sido inicializado.
+  /// </exception>
   public static MineField? Deserialize()
   {
-    string deseralization = File.ReadAllText(_instance.Filename);
+    if (s_instance is null)
+    {
+      throw new InvalidOperationException("O serializador não foi inicializado.");
+    }
+
+    string deseralization = File.ReadAllText(s_instance.Filename);
 
     return JsonSerializer.Deserialize<MineField>(deseralization);
   }
