4207c0520d9f58407da284d1ddc88a66f3d5ae07
Zerocentos <a2025950912@teiacoltec.org>
2026-08-22 23:35:07 -0300

    Adiciona UID version na classe MineField
    Serve para caso a desserialização de uma classe desatualizada
    não dê problema com a classe atualizada

--- src/MineField.cs
+++ src/MineField.cs
@@ -10,6 +10,15 @@ namespace CampoMinado.Core;
 public class MineField
 {
   /// <summary>
+  /// Versão da classe
+  /// </summary> 
+  /// <remarks> 
+  /// Serve para diferenciar as classes na desserialização
+  /// fazendo com que não haja conflito.
+  /// </remarks>
+  private readonly static string UIDVersion = "1.0";
+
+  /// <summary>
   /// Quantidade padrão de bombas por célula.
   /// </summary>
   public const double DEFAULT_BOMB_DENSITY = 0.3;
